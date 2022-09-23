1b83f965582b43c308033b79d6c9fa1ccd18c635
Egor <ee3068316@gmail.com>
2022-09-23 11:37:43 +0300

    add ex1

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите числа массива через пробел");
+int[] arr_user = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < arr_user.Length; i++)
+{
+    if(arr_user[i] > 0)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine($"Количество элементов > 0 = {count}");
+
+
+
